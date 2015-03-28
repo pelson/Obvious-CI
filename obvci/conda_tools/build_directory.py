6cb8dc254eee233e3a44a574d0e37263796c0192
Phil Elson <pelson.pub@gmail.com>
2015-03-28 12:21:24 +0000

    Added command line flag to change the class at runtime.

--- obvci/conda_tools/build_directory.py
+++ obvci/conda_tools/build_directory.py
@@ -6,6 +6,8 @@ Once a build is complete, the distribution will be uploaded (provided BINSTAR_TO
 defined), and the next package will be processed.
 
 """
+from __future__ import print_function
+
 import os
 import subprocess
 from argparse import Namespace
@@ -85,55 +87,80 @@ def sort_dependency_order(metas):
     return sorted(metas, key=lambda meta: sorted_names.index(meta.name()))
 
 
-def main(conda_recipes_root, upload_owner, upload_channel):
-    """
-    Build a directory of conda recipes sequentially, if they don't already exist on the owner's binstar account.
-    If the build does exist on the binstar account, but isn't in the targeted channel, it will be added to upload_channel,
-    All built distributions will be uploaded to the owner's channel.
+class Builder(object):
+    def __init__(self, conda_recipes_root, upload_owner, upload_channel):
+        """
+        Build a directory of conda recipes sequentially, if they don't already exist on the owner's binstar account.
+        If the build does exist on the binstar account, but isn't in the targeted channel, it will be added to upload_channel,
+        All built distributions will be uploaded to the owner's channel.
 
-    Note: Recipes may not compute their version/build# at build time.
+        Note: Recipes may not compute their version/build# at build time.
 
-    """
-    conda_recipes_root = os.path.abspath(os.path.expanduser(conda_recipes_root))
-    recipe_metas = fetch_metas(conda_recipes_root)
-    recipe_metas = sort_dependency_order(recipe_metas)
+        """
+        self.conda_recipes_root = conda_recipes_root
+        self.upload_owner = upload_owner
+        self.upload_channel = upload_channel
 
-    binstar_token = os.environ.get('BINSTAR_TOKEN', None)
-    can_upload = binstar_token is not None
+        self.binstar_token = os.environ.get('BINSTAR_TOKEN', None)
+        self.can_upload = self.binstar_token is not None
 
-    if not can_upload:
-        print('**Build will continue, but no uploads will take place.**')
-        print('To automatically upload from this script, define the BINSTAR_TOKEN env variable.')
-        print('This is done automatically on the travis-ci system once the PR has been merged.')
-    else:
-        print('conda build currently leaks all environment variables on windows, therefore the BINSTAR_TOKEN '
-              'is being reset. See https://github.com/conda/conda-build/pull/274 for progress.')
-        os.environ['BINSTAR_TOKEN'] = 'Hidden by Obvious-CI'
+        if not self.can_upload:
+            print('**Build will continue, but no uploads will take place.**')
+            print('To automatically upload from this script, define the BINSTAR_TOKEN env variable.')
+            print('This is done automatically on the travis-ci system once the PR has been merged.')
+        else:
+            print('conda build currently leaks all environment variables on windows, therefore the BINSTAR_TOKEN '
+                  'is being reset. See https://github.com/conda/conda-build/pull/274 for progress.')
+            os.environ['BINSTAR_TOKEN'] = 'Hidden by Obvious-CI'
 
-    binstar_cli = get_binstar(Namespace(token=binstar_token, site=None))
+        self.binstar_cli = get_binstar(Namespace(token=self.binstar_token, site=None))
 
-    # Figure out which distributions binstar.org already has.
-    existing_distributions = [meta for meta in recipe_metas
-                              if inspect_binstar.distribution_exists(binstar_cli, upload_owner, meta)]
+    def fetch_all_metas(self):
+        """
+        Return the conda recipe metas, in the order they should be built.
 
-    print('Resolved dependencies, will be built in the following order: \n\t{}'.format(
-               '\n\t'.join(['{} (will be built: {})'.format(meta.name(), meta not in existing_distributions)
-                            for meta in recipe_metas])))
+        """
+        conda_recipes_root = os.path.abspath(os.path.expanduser(self.conda_recipes_root))
+        recipe_metas = fetch_metas(conda_recipes_root)
+        recipe_metas = sort_dependency_order(recipe_metas)
+        return recipe_metas
 
-    for meta in recipe_metas:
-        build_dist = meta not in existing_distributions
-        if build_dist:
-            print('Building ', meta.name())
-            build.build(meta)
-        if can_upload:
-            already_on_channel = inspect_binstar.distribution_exists_on_channel(binstar_cli, upload_owner, meta, channel=upload_channel)
-            if not build_dist and not already_on_channel:
+    def calculate_existing_distributions(self, recipe_metas):
+        # Figure out which distributions binstar.org already has.
+        existing_distributions = [meta for meta in recipe_metas
+                                  if inspect_binstar.distribution_exists(self.binstar_cli, self.upload_owner, meta)]
+
+        print('Resolved dependencies, will be built in the following order: \n\t{}'.format(
+                   '\n\t'.join(['{} (will be built: {})'.format(meta.name(), meta not in existing_distributions)
+                                for meta in recipe_metas])))
+        return existing_distributions
+
+    def build(self, meta):
+        print('Building ', meta.name())
+        build.build(meta)
+
+    def main(self):
+        recipe_metas = self.fetch_all_metas()
+        existing_distributions = self.calculate_existing_distributions(recipe_metas)
+        for meta in recipe_metas:
+            build_dist = meta not in existing_distributions
+            if build_dist:
+                self.build(meta)
+            self.post_build(meta, build_occured=build_dist)
+
+    def post_build(self, meta, build_occured=True):
+        if self.can_upload:
+            already_on_channel = inspect_binstar.distribution_exists_on_channel(self.binstar_cli,
+                                                                                self.upload_owner,
+                                                                                meta,
+                                                                                channel=self.upload_channel)
+            if not build_occured and not already_on_channel:
                 # Link a distribution.
-                print('Adding existing {} to the {} channel.'.format(meta.name(), upload_channel))
-                inspect_binstar.add_distribution_to_channel(binstar_cli, upload_owner, meta, channel=upload_channel)
+                print('Adding existing {} to the {} channel.'.format(meta.name(), self.upload_channel))
+                inspect_binstar.add_distribution_to_channel(self.binstar_cli, self.upload_owner, meta, channel=self.upload_channel)
             elif already_on_channel:
-                print('Nothing to be done for {} - it is already on {}.'.format(meta.name(), upload_channel))
+                print('Nothing to be done for {} - it is already on {}.'.format(meta.name(), self.upload_channel))
             else:
                 # Upload the distribution
-                print('Uploading {} to the {} channel.'.format(meta.name(), upload_channel))
-                build.upload(binstar_cli, meta, upload_owner, channels=[upload_channel])
+                print('Uploading {} to the {} channel.'.format(meta.name(), self.upload_channel))
+                build.upload(self.binstar_cli, meta, self.upload_owner, channels=[self.upload_channel])
