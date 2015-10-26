1d0d806e7d7e184bc0b5e66cac2486d8f7e432fe
Phil Elson <pelson.pub@gmail.com>
2015-10-26 07:07:49 +0000

    Simplified the fix.

--- obvci/conda_tools/build_directory.py
+++ obvci/conda_tools/build_directory.py
@@ -81,8 +81,8 @@ def sort_dependency_order(metas):
     meta_named_deps = {}
     buildable = [meta.name() for meta in metas]
     for meta in metas:
-        all_deps = (meta.get_value('requirements/run', []) +
-                    meta.get_value('requirements/build', []))
+        all_deps = ((meta.get_value('requirements/run', []) or []) +
+                    (meta.get_value('requirements/build', []) or []))
         # Remove version information from the name.
         all_deps = [dep.split(' ', 1)[0] for dep in all_deps]
         meta_named_deps[meta.name()] = [dep for dep in all_deps if dep in buildable]
