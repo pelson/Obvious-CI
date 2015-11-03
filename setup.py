85691cc9baae2279f16aef9c931a6e1d2c12baca
Phil Elson <pelson.pub@gmail.com>
2015-11-03 07:22:07 +0000

    Use versioneer.

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
-from setuptools import setup
 import os.path
+import sys
 
+from setuptools import setup
 
-tl_package = 'obvci'
-vn_context, vn_fname = {}, os.path.join(tl_package, '_version.py')
-try:
-    with open(vn_fname, 'r') as fh:
-        exec(fh.read(), vn_context)
-    version = vn_context.get('__version__', 'dev')
-except IOError:
-    version = 'dev'
+# Add the CWD to the path so that we get an appropriate versioneer.
+sys.path.insert(0, './')
+import versioneer
 
 
 setup(name='Obvious-ci',
-      version=version,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='Utilities to simplify CI with tools such as travis-ci and appveyor.',
       author='Phil Elson',
       author_email='pelson.pub@gmail.com',
       url='https://github.com/pelson/Obvious-ci',
       scripts=[os.path.join('scripts', script) for script in
                ['obvci_install_conda_build_tools.py', 'obvci_install_miniconda.ps1',
-                'obvci_install_miniconda.sh', 'obvci_appveyor_python_build_env.cmd',
-                'obvci_conda_build_dir.py']],
+                'obvci_install_miniconda.sh', 'obvci_appveyor_python_build_env.cmd']],
       packages=['obvci', 'obvci.conda_tools', 'obvci.cli'],
       entry_points={
           'console_scripts': [
